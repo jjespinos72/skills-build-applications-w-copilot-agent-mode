caaf7d03d97b1a949e5ff444def112eb6a589ee4
Javier Espinós <jose.espinos.marcos@accenture.com>
2026-06-23 11:26:10 +0200

    Add vibrant gradient styling to App.css, create OctoFit logo and favicon SVGs, and update navbar with logo

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -28,7 +28,8 @@ function App() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
-              🐙 OctoFit Tracker
+              <img src="/octofit-logo.svg" alt="OctoFit Tracker" />
+              OctoFit Tracker
             </Link>
             <button
               className="navbar-toggler"
